ca3cf3f257422b57d63e24a48713ba8fa505da22
laozf <3302816060@qq.com>
2025-11-29 21:13:14 +0800

    feat：添加猜你喜欢组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { XtxGuessInstance } from '@/types/component'
 import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
@@ -7,9 +8,17 @@ import HotPanel from './components/HotPanel.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 
+// 轮播图数据
 const bannerList = ref<BannerItem[]>([])
+// 分类数据
 const categoryList = ref<CategoryItem[]>([])
+// 热门推荐数据
 const hotList = ref<HotItem[]>([])
+//获取猜你喜欢组件实例
+const guessRef = ref<XtxGuessInstance>()
+//下拉刷新状态
+const isTriggered = ref(false)
+
 // 获取首页轮播图数据
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
@@ -25,6 +34,22 @@ const getHomeHotData = async () => {
   const res = await getHomeHotAPI()
   hotList.value = res.result
 }
+
+//滚动触底事件
+const onScrolltolower = () => {
+  console.log('滚动触底')
+  guessRef.value?.getMore()
+}
+// 自定义下拉刷新被触发
+const onRefresherrefresh = async () => {
+  // 开启动画
+  isTriggered.value = true
+  // 重置猜你喜欢组件数据
+  guessRef.value?.resetData() // 加载数据
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()]) // 关闭动画
+  isTriggered.value = false
+}
+
 // 页面加载时获取数据
 onLoad(() => {
   getHomeBannerData()
@@ -34,20 +59,36 @@ onLoad(() => {
 </script>
 
 <template>
-  <view>
-    <!-- 导航栏 -->
-    <CustomNavbar />
+  <!-- 导航栏 -->
+  <CustomNavbar />
+  <!-- 滚动容器 -->
+  <scroll-view
+    refresher-enabled
+    @refresherrefresh="onRefresherrefresh"
+    :refresher-triggered="isTriggered"
+    class="scroll-view"
+    scroll-y
+    @scrolltolower="onScrolltolower"
+  >
     <!-- 轮播图 -->
     <XtxSwiper :list="bannerList" />
     <!-- 分类面板 -->
     <CategoryPanel :list="categoryList" />
     <!-- 热门推荐 -->
     <HotPanel :list="hotList" />
-  </view>
+    <!-- 猜你喜欢 -->
+    <XtxGuess ref="guessRef" />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
