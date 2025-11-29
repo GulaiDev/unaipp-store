81238265e77c5b06d2d252bad0a28c1cc6f9783e
laozf <3302816060@qq.com>
2025-11-29 19:58:18 +0800

    feat:添加首页分类

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,28 +1,42 @@
 <script setup lang="ts">
-import type { BannerItem } from '@/types/home'
-import { getHomeBannerAPI } from '@/services/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 
 const bannerList = ref<BannerItem[]>([])
+const categoryList = ref<CategoryItem[]>([])
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
+
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
 <template>
-  <view class="index">
+  <view>
+    <!-- 导航栏 -->
     <CustomNavbar />
+    <!-- 轮播图 -->
     <XtxSwiper :list="bannerList" />
-    <view>indexffdf</view>
+    <!-- 分类面板 -->
+    <CategoryPanel :list="categoryList" />
   </view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background: #f7f7f7;
+}
 </style>
